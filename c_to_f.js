26053547b0d18064d643c5f512e848ab39eb0f3f
Johanny Clase <jocodes28@gmail.com>
2021-05-21 11:37:47 -0400

    added styling and button functionality

---
+++ c_to_f.js
@@ -0,0 +1,16 @@
+"use strict"
+
+window.onload = function() {
+    const convertBtn = document.getElementById("convertBtn");
+    convertBtn.onclick = convertBtnClicked;
+}
+
+function convertBtnClicked() {
+    const celsiusField = document.getElementById("celsius");
+    let celsius = Number(celsiusField.value);
+
+    let answer = ((celsius * 1.8) + 32);
+
+    const answerField = document.getElementById("answer");
+    answerField.value = answer.toFixed(2);
+}
---
+++ f_to_c.js
@@ -0,0 +1,16 @@
+"use strict"
+
+window.onload = function() {
+    const convertBtn = document.getElementById("convertBtn");
+    convertBtn.onclick = convertBtnClicked;
+}
+
+function convertBtnClicked() {
+    const farenheitField = document.getElementById("farenheit");
+    let farenheit = Number(farenheitField.value);
+
+    let answer = ((farenheit - 32) * 0.55555555555);
+
+    const answerField = document.getElementById("answer");
+    answerField.value = answer.toFixed(2);
+}
